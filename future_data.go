be5222ac4136bee7b56c089b96d64082957e75eb
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-09-05 19:26:30 +0430

    Add: Ping

--- future_data.go
+++ future_data.go
@@ -1,6 +1,10 @@
 package gocoinex
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
 type FutureDataClient struct {
 	client *http.Client
@@ -10,8 +14,33 @@ func NewFutureDataClient() *FutureDataClient {
 	return &FutureDataClient{client: &http.Client{}}
 }
 
-//
-func (c *FutureDataClient) Ping() {}
+func (c *FutureDataClient) get(endpoint string, params Map) (*http.Response, error) {
+	route, err := url.Parse(BaseURL + endpoint)
+	query := route.Query()
+	if err != nil {
+		return nil, err
+	}
+	if params != nil {
+		for k, v := range params {
+			query.Set(k, fmt.Sprintf("%v", v))
+		}
+		route.RawQuery = query.Encode()
+	}
+	request, err := http.NewRequest("GET", route.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("User-Agent", UserAgent)
+	return c.client.Do(request)
+}
+
+func (c *FutureDataClient) Ping() (*Ping, error) {
+	raw_response, err := c.get("https://api.coinex.com/perpetual/v1/ping", nil)
+	if err != nil {
+		return nil, err
+	}
+	return (&Ping{}).Parse(raw_response)
+}
 
 //
 func (c *FutureDataClient) GetSystemTime() {}
