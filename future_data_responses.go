ae32d08a92c1b4e4432996608762a4d186bb7d88
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-03-04 12:07:09 +0330

    Update: SubmitLimitOrder

--- future_data_responses.go
+++ future_data_responses.go
@@ -275,6 +275,12 @@ type SubmitLimitOrder struct {
 	Position_type    int     `json:"position_type"`    // Margin type, 1: Isolated, 2: Cross
 }
 
+func (r *SubmitLimitOrder) Parse(raw_response *http.Response) (*SubmitLimitOrder, error) {
+	err := json.NewDecoder(raw_response.Body).Decode(r)
+	defer raw_response.Body.Close()
+	return r, err
+}
+
 type SubmitMarketOrder struct {
 	Order_id         int     `json:"order_id"`         // Order id
 	Position_id      int     `json:"position_id"`      // Position id
