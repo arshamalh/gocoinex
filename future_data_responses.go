aaa66bec0808c58c4bbbd4e10f189f125d583717
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-03-04 11:41:23 +0330

    Update: Ping

--- future_data_responses.go
+++ future_data_responses.go
@@ -1,8 +1,19 @@
 package gocoinex
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type Ping struct {
 	Data string `json:"data"` // Pong
 
+}
+
+func (r *Ping) Parse(raw_response *http.Response) (*Ping, error) {
+	err := json.NewDecoder(raw_response.Body).Decode(r)
+	defer raw_response.Body.Close()
+	return r, err
 }
 
 type SystemTime struct {
